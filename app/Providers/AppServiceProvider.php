5a630822834f1bd6f640e65f80cd88b7f0099826
Ravinder Kaur <rkaur@knowyourdosh.com>
2025-08-12 20:53:27 +0530

    register login push

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +21,8 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
